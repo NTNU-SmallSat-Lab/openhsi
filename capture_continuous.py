e26305c64a50f3b7836c3d36f5107d869d52c3f0
leoguen <leoguenzel@gmail.com>
2025-01-10 14:43:33 +0100

    Initial Upload

--- capture_continuous.py
+++ capture_continuous.py
@@ -1,12 +1,31 @@
 import os
 from openhsi.capture import *
 from openhsi.cameras import IDSCamera
+import time
 
-json_path= '/home/ubuntu/Documents/openhsi/nbs/assets/cam_settings_ids.json'
-with IDSCamera(n_lines=5, 
-                 processing_lvl = -1,
-                 json_path = json_path) as cam:
-    cam.collect()
+json_path= '/home/ubuntu/Documents/openhsi/capture_continuous_ids_settings.json'
+
     
-    fig = cam.show(hist_eq=True)
-    
+with IDSCamera(n_lines=10, 
+        processing_lvl = -1,
+        json_path = json_path) as cam:
+        cam.collect()
+
+
+#cam.save(save_dir="/home/ubuntu/OpenHSIcubes")
+# Define the bands for RGB channels (adjust indices based on your wavelength calibration)
+rgb_bands = {
+    'r': 30,  # Index for the red channel
+    'g': 20,  # Index for the green channel
+    'b': 10   # Index for the blue channel
+}
+
+
+cam.save(
+    save_dir="/home/ubuntu/OpenHSIcubes",
+    prefix='experiment1_',
+    #rgb_bands=rgb_bands,
+    #robust=True
+)
+fig = cam.show(plot_lib="matplotlib", robust=True)
+
